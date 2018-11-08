211eca368ec826b8b44937e6e9d26d0246ce0025
David Flynn <dflynn@blackberry.com>
2018-11-09 00:17:16 +0100

    release v0.12.3

--- source/pcc_distortion.hpp
+++ source/pcc_distortion.hpp
@@ -57,7 +57,7 @@ using namespace nanoflann;
 using namespace std;
 using namespace pcc_processing;
 
-#define PCC_QUALITY_VERSION "0.12.2"
+#define PCC_QUALITY_VERSION "0.12.3a"
 
 namespace pcc_quality {
 

1392af4d9c597c95125343712835af2fcec84590
Julien Ricard <julien.ricard@interdigital.com>
2023-01-27 11:20:00 +0100

    Release 0.14.1

--- source/pcc_distortion.hpp
+++ source/pcc_distortion.hpp
@@ -57,7 +57,7 @@ using namespace nanoflann;
 using namespace std;
 using namespace pcc_processing;
 
-#define PCC_QUALITY_VERSION "0.14.0"
+#define PCC_QUALITY_VERSION "0.14.1"
 
 namespace pcc_quality {
 
